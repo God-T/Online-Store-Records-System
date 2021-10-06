aafcde27dc0cc50a9f85e349b824e1c8ff3bebeb
shuyu Zong <41532124+shuyuZong@users.noreply.github.com>
2021-10-07 10:43:18 +1100

    Set up from create-react-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+export const App = () => (
+	<div className="App">
+		<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+			<a class="navbar-brand" href="#">
+				Online Store Records App
+			</a>
+			<button
+				class="navbar-toggler"
+				type="button"
+				data-toggle="collapse"
+				data-target="#navbarSupportedContent"
+				aria-controls="navbarSupportedContent"
+				aria-expanded="false"
+				aria-label="Toggle navigation"
+			>
+				<span class="navbar-toggler-icon"></span>
+			</button>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item active my-2 my-lg-0">
+						<a class="nav-link" href="#">
+							Customers <span class="sr-only ">(current)</span>
+						</a>
+					</li>
+					<li class="nav-item active my-2 my-lg-0">
+						<a class="nav-link" href="#">
+							Products <span class="sr-only ">(current)</span>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	</div>
+);
